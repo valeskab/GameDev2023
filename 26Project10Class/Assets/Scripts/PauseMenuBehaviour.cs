f1a41c9c8381992ec6e4204677c3a54e28a456f7
Valeska Bagaric <valeskabagaric@hotmail.com>
2023-02-13 19:12:44 -0700

    Added Audiomanager

---
+++ 26Project10Class/Assets/Scripts/PauseMenuBehaviour.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class PauseMenuBehaviour : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
