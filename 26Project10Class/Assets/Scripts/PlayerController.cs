431199a96db6d4a1ea50f51b49423d456f85a7c1
Valeska Bagaric <valeskabagaric@hotmail.com>
2023-02-28 23:47:19 -0700

    Added new game mode

--- 26Project10Class/Assets/Scripts/PlayerController.cs
+++ 26Project10Class/Assets/Scripts/PlayerController.cs
@@ -1,18 +1,12 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void Update()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        throw new NotImplementedException();
     }
 }
