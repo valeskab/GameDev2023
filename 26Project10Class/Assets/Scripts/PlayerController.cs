e49b0c6d415e7ef9d5331bf0afc0be4f7e710353
Valeska <valeskabagaric@hotmail.com>
2023-03-20 20:49:07 -0600

    Fixed the character controller

--- 26Project10Class/Assets/Scripts/PlayerController.cs
+++ 26Project10Class/Assets/Scripts/PlayerController.cs
@@ -5,8 +5,34 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] private float moveSpeed = 10f;
     private void Update()
     {
-        throw new NotImplementedException();
+        Vector2 inputVector = new Vector2(0, 0);
+        if (Input.GetKey(KeyCode.W))
+        {
+            inputVector.y = +1;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            inputVector.y = -1;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            inputVector.x = -1;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            inputVector.x = +1;
+        }
+
+        inputVector = inputVector.normalized;
+
+        Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
+        transform.position += moveDir * moveSpeed * Time.deltaTime;
+
+        float rotateSpeed = 10f;
+
+        transform.forward = Vector3.Slerp(transform.forward,moveDir, Time.deltaTime * rotateSpeed);
     }
 }
