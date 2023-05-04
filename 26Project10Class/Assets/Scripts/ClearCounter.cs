2f5609f6d6915ddb76228947fe54b32b1d09c160
Valeska <valeskabagaric@hotmail.com>
2023-05-03 20:51:13 -0600

    Added Asset

--- 26Project10Class/Assets/Scripts/ClearCounter.cs
+++ 26Project10Class/Assets/Scripts/ClearCounter.cs
@@ -3,12 +3,14 @@ using UnityEngine;
 public class ClearCounter : MonoBehaviour
 {
 
-    [SerializeField] private Transform tomatoPrefab;
+    [SerializeField] private KitchenObjectSO kitchenObjectSO;
     [SerializeField] private Transform counterTopPoint;
     public void Interact()
     {
         Debug.Log("Interact!pls");
-        Transform tomatoTransform = Instantiate(tomatoPrefab, counterTopPoint);
-        tomatoTransform.localPosition = Vector3.zero;
+        Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab, counterTopPoint);
+        kitchenObjectTransform.localPosition = Vector3.zero;
+
+        Debug.Log(kitchenObjectTransform.GetComponent<KitchenObject>().GetKitchenObjectSO().objectName);
     }
 }
